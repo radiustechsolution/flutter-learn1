7bcfa0336f39007bf5d60754e376569390ec52fb
Oludowole Olumide <xeonncodes@gmail.com>
2024-12-10 05:22:25 +0100

    Navigation 2

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,6 +8,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.blue[100], body: Center(child: Text("Home")));
+        backgroundColor: Colors.blue[100],
+        body: Center(child: Text("Home Page")));
   }
 }
